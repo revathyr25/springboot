44caf73132feead00a633330ef4dd47d3e6485a0
revathyr <revathyr@revathys-air.dlink>
2021-05-29 20:47:30 +0530

    First commit

---
+++ springboot-backend/src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -0,0 +1,78 @@
+package com.example.springboot.controller;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot.exception.ResourceNotFoundException;
+import com.example.springboot.model.Employee;
+import com.example.springboot.repository.EmployeeRepository;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+	//create employee rest api
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	//get employee by id rest api
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeId(@PathVariable Long id) {
+		
+		Employee employee= employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Employee not found with id: "+id));
+		return ResponseEntity.ok(employee);
+	}
+	
+	//update employee rest api
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails){
+		Employee employee= employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Employee not found with id: "+id));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+		
+		Employee updatedemployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedemployee);
+	}
+	
+	//delete employee rest api
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee employee= employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Employee not found with id: "+id));
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+	
+}
+
